b4c2d770f598f4002658986e49354cceab4ba3f3
ADITYA NIRANJAN <Adityaniranjan964@gmail.com>
2025-12-22 16:15:40 +0530

    Refactor TSP implementation and variable initialization

--- Travelling_sales_man_TSP.c
+++ Travelling_sales_man_TSP.c
@@ -1,40 +1,46 @@
+// Online C compiler to run C program online
 #include <stdio.h>
 
-int cost[50][50],v[10],city,min_cost,min;
-int count=0;
-int next_city=-1;
+int adj[50][50],count=0,next_city=0,v[10]={0},city;
+int min_cost=0;
+
+
+
 void tsp(int c){
     int min=999;
-
+    
     v[c]=1;
-    printf("%d-> ",c);
-
+    printf("%d->",c);
+    
     if(count==city-1){
-        min_cost+=cost[c][0];
         printf("0");
-        printf(" min cost:%d",min_cost);
+        min_cost+=adj[c][0];
+        printf("\n min cost to reach the path:%d",min_cost);
         return;
     }
-
+    
     for(int i=0;i<city;i++){
-        if(v[i]==0 && cost[c][i]!=0 && cost[c][i]<min){
-            min=cost[c][i];
+        if(adj[c][i]!=0 && v[i]==0 && adj[c][i]<min){
             next_city=i;
-
+            min=adj[c][i];
         }
     }
     min_cost+=min;
     count++;
     tsp(next_city);
-
+    
+    
 }
-
-int main(){
+int main() {
     city=4;
     for(int i=0;i<city;i++){
         for(int j=0;j<city;j++){
-            scanf("%d",&cost[i][j]);
+            scanf("%d",&adj[i][j]);
         }
     }
     tsp(0);
+    // Write C code here
+
+
+    return 0;
 }
